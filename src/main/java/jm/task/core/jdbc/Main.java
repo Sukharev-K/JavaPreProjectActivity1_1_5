d80a9a358c290aed90b28003efec7f05a6efdbcb
Sukharev-K <kostia.sukharev.a@gmail.com>
2023-10-12 20:03:29 +0300

    Практическая задача 1.1.5

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,6 +5,11 @@ import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.List;
 
@@ -13,6 +18,7 @@ public class Main {
         // реализуйте алгоритм здесь
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
+
 
         User user1 = new User("Konstantin", "Sukharev", (byte) 26);
         userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
@@ -38,6 +44,7 @@ public class Main {
         userService.cleanUsersTable();
         userService.dropUsersTable();
 
+        //Util.getSessionFactory();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,12 @@
 package jm.task.core.jdbc.dao;
 
+import com.mysql.cj.xdevapi.SqlResultBuilder;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
+
 
 import java.util.List;
 
@@ -12,31 +18,98 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Session session = Util.getSessionFactory().getCurrentSession();
+
+        try {
+            session.beginTransaction();
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS hibernate_db.User (" +
+                    "id BIGINT AUTO_INCREMENT PRIMARY KEY," +
+                    "name VARCHAR(255)," +
+                    "lastName VARCHAR(255)," +
+                    "age TINYINT" +
+                    ")";
+            //Я не смог найти не устаревшего метода для того чтобы
+            // создать таблицу с помощью SQL запроса....
+            NativeQuery query = session.createNativeQuery(createTableSQL);
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
 
     }
 
     @Override
     public void dropUsersTable() {
+        Session session = Util.getSessionFactory().getCurrentSession();
+        try {
+            session.beginTransaction();
 
+            String dropTableSQL = "DROP TABLE IF EXISTS hibernate_db.User";
+            NativeQuery query = session.createNativeQuery(dropTableSQL);
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        try (Session session = Util.getSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
+
+            User user = new User(name, lastName, age);
+            session.persist(user);
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = Util.getSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
 
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.remove(user);
+            }
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList;
+        try (Session session = Util.getSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
+            userList = session.createQuery("Select u From User u", User.class)
+                            .getResultList();
+            session.getTransaction().commit();
+        }
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
+        //List<User> userList;
+        try (Session session = Util.getSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
 
+            session.createQuery("DELETE FROM User").executeUpdate();
+
+
+            session.getTransaction().commit();
+        }
     }
 }
